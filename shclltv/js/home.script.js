5a5714ba44ede8640192505c603663b1a14b1ebd
Samuel Snopko <samuel@wondrous.ch>
2017-06-18 21:42:06 +0200

    shclltv demo update

--- shclltv/js/home.script.js
+++ shclltv/js/home.script.js
@@ -3,31 +3,46 @@ const initHome = function() {
 
   if (document.querySelector('.home-offer')) {
     var $homeOffer = $('.home-offer');
-    $homeOffer.on('click', 'a:not(.choosed)', function(event) {
+    $homeOffer.on('click', 'li:not(.choosed) a', function(event) {
       event.preventDefault();
-      $('a.choosed').removeClass('choosed');
-      $(this).addClass('choosed');
+      $('li.choosed a').off('click');
+      $('li.choosed').removeClass('choosed');
+      $(this).closest('li').addClass('choosed');
       $homeOffer.children('.' + $(this).data('lightbox')).addClass('showed').siblings().removeClass('showed');
-    });
-    if (Foundation.MediaQuery.atLeast('large')) {
-      $homeOffer.on('mouseover', 'a:not(.choosed)', function(event) {
+      $(this).on('click', function(event){
         event.preventDefault();
-        $('a.choosed').removeClass('choosed');
-        $(this).addClass('choosed');
+        var $this= $(this);
+        console.log('click');
+        $this.closest('li').addClass('loading');
+        setTimeout(function() {
+          lity($this.attr('href'));
+          $this.closest('li').removeClass('loading');
+        }, 0);
+      });
+    });
+    if (Foundation.MediaQuery.atLeast('xlarge')) {
+      $homeOffer.on('mouseover', 'li:not(.choosed) a', function(event) {
+        event.preventDefault();
+        $('li.choosed a').off('click');
+        $('li.choosed').removeClass('choosed');
+        $(this).closest('li').addClass('choosed');
         $('.choose').addClass('visually-hidden');
         $homeOffer.children('.' + $(this).data('lightbox')).addClass('showed').siblings().removeClass('showed');
+        $(this).on('click', function(event){
+          event.preventDefault();
+          var $this= $(this);
+          console.log('click');
+          $this.closest('li').addClass('loading');
+          setTimeout(function() {
+            lity($this.attr('href'));
+            $this.closest('li').removeClass('loading');
+          }, 1500);
+        });
+      });
+      $homeOffer.on('mouseleave', 'li.choosed a', function(event) {
+        $(this).closest('li').removeClass('choosed');
       });
     }
-    // $homeOffer.on('click', 'a.choosed[data-lightbox]', lity);
-    $homeOffer.on('click', 'a.choosed[data-lightbox]', function(event) {
-      event.preventDefault();
-      var $this= $(this);
-      $this.find('.label').addClass('loading');
-      setTimeout(function() {
-        lity($this.attr('href'));
-        $this.find('.label').removeClass('loading');
-      }, 1000);
-    });
     console.log('Home ignited');
   }
 }
