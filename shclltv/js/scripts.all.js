694d4426ec60271cdc00e56b40007670431a2bef
Samuel Snopko <samuel@wondrous.ch>
2017-06-26 11:37:16 +0200

    shy update

--- shclltv/js/scripts.all.js
+++ shclltv/js/scripts.all.js
@@ -899,7 +899,7 @@ const initHome = function() {
           setTimeout(function() {
             lity($this.attr('href'));
             $this.closest('li').removeClass('loading');
-          }, 1500);
+          }, 2000);
         });
       });
       $homeOffer.on('mouseleave', 'li.choosed a', function(event) {
@@ -939,6 +939,44 @@ const initProjects = function() {
     //   event.preventDefault();
     //   // smoothState.load('./niro-project-v1.html');
     // });
+
+    // Select all links with hashes
+    $('a[href*="#"]')
+      // Remove links that don't actually link to anything
+      .not('[href="#"]')
+      .not('[href="#0"]')
+      .click(function(event) {
+        // On-page links
+        if (
+          location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+          &&
+          location.hostname == this.hostname
+        ) {
+          // Figure out element to scroll to
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+          // Does a scroll target exist?
+          if (target.length) {
+            // Only prevent default if animation is actually gonna happen
+            event.preventDefault();
+            $('html, body').animate({
+              scrollTop: target.offset().top
+            }, 1000, function() {
+              // Callback after animation
+              // Must change focus!
+              var $target = $(target);
+              $target.focus();
+              if ($target.is(":focus")) { // Checking if the target was focused
+                return false;
+              } else {
+                $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+                $target.focus(); // Set focus again
+              };
+            });
+          }
+        }
+      });
+
     console.log('Projects ignited');
   }
 }
